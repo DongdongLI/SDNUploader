52c756beb1135487826e6df6455ec7f63b3d59cf
Andy <lidongdong559@hotmail.com>
2018-02-22 16:24:38 -0500

    update SDNReader

--- src/main/java/SDNReader.java
+++ src/main/java/SDNReader.java
@@ -23,7 +23,7 @@ public class SDNReader {
 		NodeList sdnList = doc.getElementsByTagName("sdnEntry");
 		System.out.println("Lenth: "+sdnList.getLength());
 
-		for(int i=0;i<1;i++){
+		for(int i=0;i<2;i++){
 			//System.out.println(sdnList.item(i).getTextContent());
 			Node node = sdnList.item(i);
 			System.out.println("\ncurrrent element: "+node.getNodeName());
@@ -36,10 +36,28 @@ public class SDNReader {
 				System.out.println("sdnType: " + eElement.getElementsByTagName("sdnType").item(0).getTextContent());
 
 				NodeList programList = eElement.getElementsByTagName("programList");
-				System.out.println("Program list: "+programList.getLength());
+				System.out.println("Program list: ");
 				for(int j=0;j<programList.getLength();j++){
-					Element program = (Element)programList.item(i);
+					Element program = (Element)programList.item(j);
 					System.out.println(program.getElementsByTagName("program").item(0).getTextContent());
+				}
+				
+				NodeList akaList = eElement.getElementsByTagName("akaList");
+				System.out.println("Aka list: ");
+				for(int j=0;j<akaList.getLength();j++){
+					Element aka = (Element)akaList.item(j);
+					System.out.println("uid: "+aka.getElementsByTagName("uid").item(0).getTextContent());
+					System.out.println("category: "+aka.getElementsByTagName("category").item(0).getTextContent());
+					System.out.println("lastName: "+aka.getElementsByTagName("lastName").item(0).getTextContent());
+				}
+				
+				NodeList addressList = eElement.getElementsByTagName("address");
+				System.out.print("Address List: ");
+				for(int j=0;j<addressList.getLength();j++){
+					Element address = (Element)addressList.item(j);
+					System.out.println("uid: "+address.getElementsByTagName("uid").item(0).getTextContent());
+					System.out.println("city: "+address.getElementsByTagName("city").item(0).getTextContent());
+					System.out.println("country: "+address.getElementsByTagName("country").item(0).getTextContent());
 				}
 			}
 		}
